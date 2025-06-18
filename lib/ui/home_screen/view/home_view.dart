22a68fde894d943df145b9e2933e789976f19795
mustafatmaca <mustafaatmaca362@gmail.com>
2025-06-18 22:38:30 +0300

    add button rework

--- lib/ui/home_screen/view/home_view.dart
+++ lib/ui/home_screen/view/home_view.dart
@@ -3,7 +3,6 @@ import 'package:atmacayapi/ui/addCategory_screen/view/addCategory_view.dart';
 import 'package:atmacayapi/ui/addProduct_screen/view/addProduct_view.dart';
 import 'package:atmacayapi/ui/home_screen/controller/home_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:get/get.dart';
 
 class HomeView extends StatelessWidget {
@@ -18,52 +17,25 @@ class HomeView extends StatelessWidget {
             "Atmaca Yapı Malzemeleri",
             style: Theme.of(context).textTheme.titleLarge,
           ),
-        ),
-        floatingActionButton: SpeedDial(
-          overlayOpacity: 0.2,
-          overlayColor:
-              MediaQuery.of(context).platformBrightness == Brightness.light
-                  ? AppColor.backgroundLight
-                  : AppColor.backgroundDark,
-          backgroundColor: Theme.of(context).primaryColor,
-          foregroundColor: AppColor.textDark,
-          activeBackgroundColor: Theme.of(context).primaryColor,
-          activeForegroundColor: AppColor.textDark,
-          icon: Icons.add,
-          activeIcon: Icons.close,
-          children: [
-            SpeedDialChild(
-              label: 'Kategori Ekle',
-              labelStyle: Theme.of(context)
-                  .textTheme
-                  .bodyLarge!
-                  .copyWith(color: AppColor.textDark),
-              labelBackgroundColor: Theme.of(context).primaryColor,
-              backgroundColor: Theme.of(context).primaryColor,
-              foregroundColor: AppColor.textDark,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Icon(Icons.library_add),
-              onTap: () {
-                Get.to(() => AddCategoryView());
-              },
-            ),
-            SpeedDialChild(
-              label: 'Ürün Ekle',
-              labelStyle: Theme.of(context)
-                  .textTheme
-                  .bodySmall!
-                  .copyWith(color: AppColor.textDark),
-              labelBackgroundColor: Theme.of(context).primaryColor,
-              backgroundColor: Theme.of(context).primaryColor,
-              foregroundColor: AppColor.textDark,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(24),
-              ),
-              child: Icon(Icons.add_box),
-              onTap: () {
-                Get.to(() => AddProductView());
+          actions: [
+            PopupMenuButton(
+              icon: const Icon(Icons.add),
+              color: AppColor.backgroundLight,
+              itemBuilder: (context) {
+                return {'Kategori', 'Ürün'}.map(
+                  (e) {
+                    return PopupMenuItem(
+                      child: Text(e),
+                      onTap: () {
+                        if (e == 'Ürün') {
+                          Get.to(() => AddProductView());
+                        } else {
+                          Get.to(() => AddCategoryView());
+                        }
+                      },
+                    );
+                  },
+                ).toList();
               },
             )
           ],
